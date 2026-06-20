1805f10943c7d287dca0157d16c35e3bd05bcf21
Asif Mohtesham <asifmohtesham@gmail.com>
2026-06-20 13:37:35 +0400

    build(visual): rebuild bundle with PDF preview tab; bump version

--- assets/js/block-editor/index.asset.php
+++ assets/js/block-editor/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '240374b50d27b9cab7d8');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '94f398c309e2ae5b15e3');
