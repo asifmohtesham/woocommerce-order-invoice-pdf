8005e5e657c00f3678d13406f82c38837c74589c
Asif Mohtesham <asifmohtesham@gmail.com>
2026-06-24 09:58:34 +0400

    chore: v1.5.78 + build

--- assets/js/block-editor/index.asset.php
+++ assets/js/block-editor/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-deprecated', 'wp-dom', 'wp-element', 'wp-i18n', 'wp-preferences', 'wp-primitives'), 'version' => '0ee7dbaf070fd7ff5444');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-deprecated', 'wp-dom', 'wp-element', 'wp-i18n', 'wp-preferences', 'wp-primitives'), 'version' => '9297ba3de3e78641f589');
